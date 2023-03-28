988a4a2324894380ab2ca0dc2d62e066511c7645
hassanelsayd <hassan22elsayd@gmail.com>
2023-03-28 20:41:55 +0200

    ready for deploy

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import Payment from "./pages/Payment";
+import Home from "./pages/Home";
+import Notifications from "./pages/Notifications";
+import OfferDetails from "./pages/OfferDetails";
+import Services from "./pages/Services";
+import PostDetails from "./pages/PostDetails";
+import Chats from "./pages/Chats";
+import { Routes, Route } from "react-router-dom";
+import "./styles/global.css";
+import Layout from "./ui/Layout";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/payment" element={<Payment />} />
+          <Route path="/notifications" element={<Notifications />} />
+          <Route path="/offer-details" element={<OfferDetails />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/post-details" element={<PostDetails />} />
+          <Route path="/chats" element={<Chats />} />
+        </Routes>
+      </Layout>
     </div>
   );
 }
